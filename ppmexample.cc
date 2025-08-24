f6c169cba04d5ca846295f0c30025c09780f4369
Liam Tait <142124471+TubaSpinach@users.noreply.github.com>
2025-08-24 16:52:35 -0400

    6.2 refactor sphere intersection

--- ppmexample.cc
+++ ppmexample.cc
@@ -5,15 +5,15 @@
 
 double hit_sphere(const point3& center, double radius, const ray& r){
     vec3 absolute_center = center - r.origin();
-    auto a = dot(r.direction(),r.direction());
-    auto b = -2.0 * dot(r.direction(),absolute_center);
-    auto c = dot(absolute_center,absolute_center) - radius*radius;
-    auto discriminant = b*b - 4*a*c;
+    auto a = r.direction().length_squared();
+    auto h = dot(r.direction(),absolute_center);
+    auto c = absolute_center.length_squared() - radius*radius;
+    auto discriminant = h*h - a*c;
 
     if (discriminant<0){
         return -1.0;
     } else {
-        return (-b - std::sqrt(discriminant))/(2.0*a);
+        return (h - std::sqrt(discriminant))/(a);
     }
 }
 
