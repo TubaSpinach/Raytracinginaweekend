e85a478ab9f933a186d2234e016003d527a6d04b
Liam Tait <142124471+TubaSpinach@users.noreply.github.com>
2025-08-24 16:31:36 -0400

    add camera and viewport

--- ppmexample.cc
+++ ppmexample.cc
@@ -3,17 +3,32 @@
 #include "color.h"
 #include "ray.h"
 
-//generate a ppm image
-// ppm is a text based image file, very simple
-// first line is encoding
-// then dimensions in pixels,
-// then color max
-// then triplets of RGB
+color ray_color(const ray& r){
+    return color(0,0,0);
+}
 
 int main() {
     //image properties
-    int image_width = 256;
-    int image_height = 256;
+    int image_width = 400;
+    auto aspect_ratio = 16.0 / 9.0;
+    int image_height = int(image_width/aspect_ratio);
+    image_height = (image_height<1) ? 1 : image_height;
+
+    auto focal_length = 1.0;
+    auto viewport_height = 2.0;
+    auto viewport_width = viewport_height * double(image_width)/image_height;
+    auto camera_center = point3(0,0,0);
+
+    vec3 viewport_u = vec3(viewport_width,0,0);
+    vec3 viewport_v = vec3(0,-viewport_height,0);
+
+    vec3 pixel_delta_u = viewport_u / image_width;
+    vec3 pixel_delta_v = viewport_v / image_height;
+
+    point3 viewport_upper_left = camera_center 
+                              -vec3(0,0,focal_length) - viewport_u/2 - viewport_v/2;
+
+    point3 pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u+pixel_delta_v);
 
     //Render
     //first three lines of file!
@@ -23,10 +38,12 @@ int main() {
         std::clog<<"\rScanlines remaining: " << (image_height-j) << ' ' << std::flush;
 
         for(int i=0;i<image_width;i++){
-            auto pixel_color = color(double(i) / (image_width-1), 
-                                     double(j) / (image_height-1),
-                                     0.0);
+            point3 pixel_center = pixel00_loc + (i*pixel_delta_u)+(j*pixel_delta_v);
+            vec3 ray_direction = pixel_center - camera_center;
+            ray r(camera_center, ray_direction);
 
+
+            color pixel_color = ray_color(r);
             write_color(std::cout,pixel_color);
         }
     }
