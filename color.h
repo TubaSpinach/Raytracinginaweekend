9b838b6a12ddbfd8f98e747236793cb748fa28d0
Liam Tait <142124471+TubaSpinach@users.noreply.github.com>
2025-08-24 15:29:54 -0400

    Add class method for writing color data to out stream

---
+++ color.h
@@ -0,0 +1,23 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#include "vec3.h"
+#include <iostream>
+
+using color=vec3;
+
+//component values assumed to be in [0,1]
+void write_color(std::ostream& out, const color& pixel_color){
+    auto r = pixel_color.x();
+    auto g = pixel_color.y();
+    auto b = pixel_color.z();
+
+    //convert to bytes
+    int ir = int(255.999)*r;
+    int ig = int(255.999*g);
+    int ib = int(255.999*b);
+
+    out<<ir<<' '<<ig<<' '<<ib<<'\n';
+}
+
+#endif
