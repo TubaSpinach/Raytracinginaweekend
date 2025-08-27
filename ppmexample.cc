7e6092200f6d2a57e3542d600982bef31f2b4225
Liam Tait <142124471+TubaSpinach@users.noreply.github.com>
2025-08-27 12:20:17 -0400

    6.7 refactor with utility header

--- ppmexample.cc
+++ ppmexample.cc
@@ -1,15 +1,12 @@
-#include <iostream>
-#include "vec3.h"
-#include "color.h"
-#include "ray.h"
-#include "sphere.h"
+#include "rtweekend.h"
 #include "hittable.h"
+#include "hittable_list.h"
+#include "sphere.h"
 
-color ray_color(const ray& r, double t){
-        
-    if(t>0.0){
-        vec3 N = unit_vector(r.at(t)-vec3(0,0,-1));
-        return 0.5*color(N.x()+1,N.y()+1,N.z()+1);
+color ray_color(const ray& r, const hittable_list& world){
+    hit_record rec;
+    if(world.hit(r,0,infinity,rec)){
+        return 0.5*(rec.normal + color(1,1,1));
     }
 
     vec3 unit_direction = unit_vector(r.direction());
@@ -24,10 +21,18 @@ int main() {
     int image_height = int(image_width/aspect_ratio);
     image_height = (image_height<1) ? 1 : image_height;
 
+    //world
+
+    hittable_list world;
+
+    world.add(make_shared<sphere>(point3(0,0,-1),0.5));
+    world.add(make_shared<sphere>(point3(0,-100.5,-1),100));
+
     //camera
     auto focal_length = 1.0;
     auto viewport_height = 2.0;
-    auto viewport_width = viewport_height * double(image_width)/image_height;
+    auto viewport_width = viewport_height * 
+                          double(image_width)/image_height;
     auto camera_center = point3(0,0,0);
 
     vec3 viewport_u = vec3(viewport_width,0,0);
@@ -37,12 +42,11 @@ int main() {
     vec3 pixel_delta_v = viewport_v / image_height;
 
     point3 viewport_upper_left = camera_center 
-                              -vec3(0,0,focal_length) - viewport_u/2 - viewport_v/2;
+                              -vec3(0,0,focal_length) 
+                              - viewport_u/2 - viewport_v/2;
 
     point3 pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u+pixel_delta_v);
 
-    //sphere
-    sphere s = sphere(point3(0,0,-1),0.5);
     //Render
     //first three lines of file!
     std::cout<< "P3\n" << image_width << ' ' << image_height << '\n' <<"255\n";
@@ -51,13 +55,13 @@ int main() {
         std::clog<<"\rScanlines remaining: " << (image_height-j) << ' ' << std::flush;
 
         for(int i=0;i<image_width;i++){
-            point3 pixel_center = pixel00_loc + (i*pixel_delta_u)+(j*pixel_delta_v);
+            point3 pixel_center = pixel00_loc 
+                                  + (i*pixel_delta_u)
+                                  + (j*pixel_delta_v);
             vec3 ray_direction = pixel_center - camera_center;
             ray r(camera_center, ray_direction);
-            hit_record k;
-            s.hit(r,0.0,1.0,k);
             
-            color pixel_color = ray_color(r,k.t);
+            color pixel_color = ray_color(r,world);
             write_color(std::cout,pixel_color);
         
             
